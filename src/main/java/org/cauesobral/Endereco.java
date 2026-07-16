3ebc16db82aec60877976a55f6e4cde673cd484d
Cauê Sobral <caueroberto56@gmail.com>
2026-07-16 17:28:42 -0300

    Criação da classe de tipo record para os dados do endereço.

---
+++ src/main/java/org/cauesobral/Endereco.java
@@ -0,0 +1,5 @@
+package org.cauesobral;
+
+public record Endereco (String cep, String logradouro, String complemento,
+                        String bairro, String localidade, String uf, String estado, String regiao){
+}
---
+++ src/main/java/org/cauesobral/Main.java
@@ -0,0 +1,7 @@
+package org.cauesobral;
+
+public class Main {
+    static void main() {
+        System.out.println("Hello World!");
+    }
+}
